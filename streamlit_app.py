77f32f07ad63456da4f7d175dc89afc69705ee1e
Dishant1992 <98050559+Dishant1992@users.noreply.github.com>
2022-06-05 14:28:19 -0400

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Bacon and Eggs')
+streamlit.text('Puri Sabzi')
+streamlit.text('Maggi')
+
+
