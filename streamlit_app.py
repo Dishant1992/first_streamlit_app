18da4097195efa1691381a22a91fdc96e2e415da
Dishant1992 <98050559+Dishant1992@users.noreply.github.com>
2022-06-05 14:30:37 -0400

    Update streamlit_app.py
    added emojis and new items

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,10 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Bacon and Eggs')
-streamlit.text('Puri Sabzi')
-streamlit.text('Maggi')
+streamlit.text(🥣 'Bacon and Eggs')
+streamlit.text(🥣 'Puri Sabzi')
+streamlit.text(🥣 'Maggi')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
