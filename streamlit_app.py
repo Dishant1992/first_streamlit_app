26e9fd7cdcc60518e226c7cb6d79e466c9fc9c62
Dishant1992 <98050559+Dishant1992@users.noreply.github.com>
2022-06-05 14:31:14 -0400

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text(🥣 'Bacon and Eggs')
-streamlit.text(🥣 'Puri Sabzi')
-streamlit.text(🥣 'Maggi')
+streamlit.text('🥣 Bacon and Eggs')
+streamlit.text('🥣 Puri Sabzi')
+streamlit.text('🥣 Maggi')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
